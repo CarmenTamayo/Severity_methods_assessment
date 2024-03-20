4623a51801c6235ef54d5fe42b7d482cb2e2c5e7
CarmenTamayo <lshct11@lshtm.ac.uk>
2024-03-20 17:52:15 +0000

    Init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' Severity_methods_assessment: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Carmen Tamayo Cuartero \email{carmen.tamayo-cuartero@lshtm.ac.uk}
+#' 
+#' @date 2024/03/20
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
