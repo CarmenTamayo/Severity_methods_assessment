c38ebe7fc1885163e39e55c9fe2f1196a7e0b458
CarmenTamayo <lshct11@lshtm.ac.uk>
2024-06-19 16:36:21 +0100

    Updating simulist use and including hosp_risk and hosp_death_risk

--- R/run_ebola_simulations.R
+++ R/run_ebola_simulations.R
@@ -11,8 +11,10 @@ run_ebola_simulations <- function(sim_count) {
     # Run the simulation code and store the result
     results[[i]] <- sim_linelist(
       contact_distribution = contact_distribution,
-      infect_period = ip_ebola_epidist,
+      infectious_period = ip_ebola_epidist,
       onset_to_hosp = NA,
+      hosp_risk = NA,
+      hosp_death_risk = NA,
       onset_to_death = o_d_ebola_epidist,
       onset_to_recovery = o_r_ebola_epidist,
       prob_infect = 0.7,
